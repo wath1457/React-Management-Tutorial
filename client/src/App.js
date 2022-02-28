c7d1d6491c8f4a1c28ec820c8858fc93e80a0f4c
wath1457 <wath1457@gmail.com>
2022-02-28 21:19:10 +0900

    Create Customer Database Table & Node.js Database Connection settings

--- client/src/App.js
+++ client/src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {this.state.customers ? this.state.customers.map(c => {return ( <Customer key = {c.id} id = {c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />); 
+            {this.state.customers ? this.state.customers.map(c => {return ( <Customer key = {c.id} id = {c.id} image={c.image} name={c.NAME} birthday={c.birthday} gender={c.gender} job={c.job} />); 
             }) : 
             <TableRow>
               <TableCell colSpan="6" align="center">
